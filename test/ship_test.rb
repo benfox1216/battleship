66b15a038910084ae5b76eb520f9fce472695135
Ben Fox <benfox1216@gmail.com>
2019-12-03 12:50:23 -0700

    Add changes to ship class to make all tests pass

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,42 +8,41 @@ class ShipTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_instance_of Ship, cruiser
+    assert_instance_of Ship, @cruiser
   end
 
   def test_it_has_a_name
-    assert_equal "Cruiser", cruiser.name
+    assert_equal "Cruiser", @cruiser.name
   end
 
   def test_ship_has_a_length
-    assert_equal 3, cruiser.length
+    assert_equal 3, @cruiser.length
   end
 
   def test_ship_has_health_equal_to_length
-    assert_equal 3, cruiser.health
-  end
-
-  def test_if_ship_can_be_sunk
-    assert_equal false, cruiser.sunk?
-    cruiser.hit
-    cruiser.hit
-    assert_equal false, cruiser.sunk?
-    cruiser.hit
-    assert_equal true, cruiser.sunk?
-    cruiser.hit
-    assert_equal true, cruiser.sunk?
+    assert_equal 3, @cruiser.health
   end
 
   def test_taking_a_hit_reduces_health
-    assert_equal 3, cruiser.health
-    cruiser.hit
-    assert_equal 2, cruiser.health
-    cruiser.hit
-    assert_equal 1, cruiser.health
-    cruiser.hit
-    assert_equal 0, cruiser.health
-    cruiser.hit
-    assert_equal 0, cruiser.health
+    assert_equal 3, @cruiser.health
+    @cruiser.hit
+    assert_equal 2, @cruiser.health
+    @cruiser.hit
+    assert_equal 1, @cruiser.health
+    @cruiser.hit
+    assert_equal 0, @cruiser.health
+    @cruiser.hit
+    assert_equal 0, @cruiser.health
   end
 
+  def test_if_ship_can_be_sunk
+    assert_equal false, @cruiser.sunk?
+    @cruiser.hit
+    @cruiser.hit
+    assert_equal false, @cruiser.sunk?
+    @cruiser.hit
+    assert_equal true, @cruiser.sunk?
+    @cruiser.hit
+    assert_equal true, @cruiser.sunk?
+  end
 end
