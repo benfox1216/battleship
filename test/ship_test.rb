054aacb4e5a851690cbd9b32235f668c89d5b8ff
Ben Fox <benfox1216@gmail.com>
2019-12-03 08:40:10 -0700

    Add name test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,12 +8,10 @@ class ShipTest < Minitest::Test
   end
 
   def test_it_exists
-    @cruiser = Ship.new("Cruiser", 3)
-
     assert_instance_of Ship, cruiser
   end
 
   def test_it_has_a_name
-
+    assert_equal "Cruiser", cruiser.name
   end
 end
