fc6ee0c7338aedda337918d201751efbd087f335
Cassie Achzenick <caachzenick@gmail.com>
2019-12-03 12:26:15 -0700

    Add ship tests

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,16 +8,42 @@ class ShipTest < Minitest::Test
   end
 
   def test_it_exists
-    @cruiser = Ship.new("Cruiser", 3)
-
     assert_instance_of Ship, cruiser
   end
 
   def test_it_has_a_name
-
+    assert_equal "Cruiser", cruiser.name
   end
 
   def test_ship_has_a_length
-    assert_equal 3, @cruiser.length
+    assert_equal 3, cruiser.length
   end
+
+  def test_ship_has_health_equal_to_length
+    assert_equal 3, cruiser.health
+  end
+
+  def test_if_ship_can_be_sunk
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    cruiser.hit
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    assert_equal true, cruiser.sunk?
+    cruiser.hit
+    assert_equal true, cruiser.sunk?
+  end
+
+  def test_taking_a_hit_reduces_health
+    assert_equal 3, cruiser.health
+    cruiser.hit
+    assert_equal 2, cruiser.health
+    cruiser.hit
+    assert_equal 1, cruiser.health
+    cruiser.hit
+    assert_equal 0, cruiser.health
+    cruiser.hit
+    assert_equal 0, cruiser.health
+  end
+
 end
