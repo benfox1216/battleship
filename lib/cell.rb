a33bc87862c275ac2a961318bf111cf8dec37367
Cassie Achzenick <caachzenick@gmail.com>
2019-12-03 15:40:01 -0700

    Add first cell interation pattern

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,2 +1,18 @@
 class Cell
+
+  attr_reader :coordinate, :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = ship
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def empty?
+    return true if @ship == nil
+    false
+  end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -1,9 +1,31 @@
 require 'Minitest/autorun'
 require 'Minitest/pride'
 require './lib/ship'
+require './lib/cell'
 
 class CellTest < Minitest::Test
   def setup
     @cruiser = Ship.new("Cruiser", 3)
+    @cell = Cell.new("B4")
+  end
+
+  def test_it_exists
+    assert_instance_of Cell, @cell
+  end
+
+  def test_cell_has_a_coordinate
+    assert_equal "B4", @cell.coordinate
+  end
+
+  def test_a_cell_can_contain_ship
+    assert_nil @cell.ship
+    @cell.place_ship(@cruiser)
+    assert_instance_of Ship, @cell.ship
+  end
+
+  def test_the_cell_is_empty
+    assert_equal true, @cell.empty?
+    @cell.place_ship(@cruiser)
+    assert_equal false, @cell.empty?
   end
 end
