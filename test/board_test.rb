119cefe6f2f6f4862e5879f0857b5abf8ab06ce1
Ben Fox <benfox1216@gmail.com>
2019-12-04 00:18:03 -0700

    Skip placement validation tests, add require for ship class

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,6 +1,7 @@
 require 'Minitest/autorun'
 require 'Minitest/pride'
 require_relative '../lib/board.rb'
+require_relative '../lib/ship.rb'
 
 class BoardTest < Minitest::Test
   def setup
@@ -20,7 +21,6 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_can_validate_coordinates
-    skip
     assert_equal true, @board.valid_coordinate?("A1")
     assert_equal false, @board.valid_coordinate?("A5")
   end
@@ -32,6 +32,7 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_validates_coordinates_are_consecutive
+    skip
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
     assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
@@ -39,11 +40,13 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_validates_coordinates_are_not_diagonal
+    skip
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
     assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
   end
 
   def test_it_validates_correct_coordinates_are_valid
+    skip
     assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
     assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
